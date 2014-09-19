4141da4471451d0bcc36e5a214a48382bc6448a7
Elliott James Perry <elliott@evvnt.com>
2014-09-19 11:11:49 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EvvntChallenge::Application.config.secret_token = 'd7d9b5138dc2d577b20656046cfe0770c380bb8720542f90d618da0e30dd10de83f247b43214298b1f581d7b18cc6af488e1185407b99be14cafb01e27bc5fb9'
