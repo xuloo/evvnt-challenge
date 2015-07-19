29c8def4d078605a23092f98540b5c6b4c2a297b
Trevor Burton-McCreadie <trevor.burton-mccreadie@tombola.com>
2015-07-19 23:24:19 +0100

    tidied up the controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,24 @@
 class EventsController < ApplicationController
 
-  # GET /events
-  # GET /events.json
+  # lists events.
+  # by default they're ordered by most recent first
+  # paging param 'p' defines the page to choose.
   def index
     result = Event.all sort: { start_time: { order: 'desc' } }, size: 10, from: params[:p]
 
     render json: { events: result, total: result.total }
   end
 
+  # returns all the events that are occurring at the venue
+  # name of the venue is supplied in the 'v' param.
   def for_venue
     result = Event.search query: { match: { 'venue.name' => params[:v] } }
 
     render json: { events: result, total: result.total }
   end
 
+  # searches by keyword
+  # keyword to search on is supplied in the 'q' param.
   def search
     result = Event.search query: { match: { 'keywords' => params[:q] } }
 
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,9 +1,11 @@
 class VenuesController < ApplicationController
 
+  # returns the list of all the venue names.
+  # the data is completely denormalized so we use an aggregation.
   def all
     results = Event.search query: { match_all: {  } }, aggregations: { venues: { terms: { field: "venue.name"}}}, size: 0
 
     render json: results.response.aggregations.venues.buckets.map { |venue| venue['key'] }
   end
-  
+
 end
