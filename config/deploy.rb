3342765edffac6bbf93f037c0277c3bc0826d9a4
Trevor Burton-McCreadie <trevor.burton-mccreadie@tombola.com>
2015-07-21 23:41:52 +0100

    deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/elk.pem) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :puma_init_active_record, false  # Change to false when not using ActiveRecord
 
 ## Defaults:
 # set :scm,           :git
